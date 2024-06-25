8161c1ae532fb43ea5804b0f6c76ba6663ff4379
leenorshn <shukurukate@gmail.com>
2024-06-25 10:03:59 +0200

    correcting R import error

--- app/src/main/java/com/avenir/rangoapp/ui/components/ProductItem.kt
+++ app/src/main/java/com/avenir/rangoapp/ui/components/ProductItem.kt
@@ -17,7 +17,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.avenir.RangoApp.R
+import com.avenir.rangoapp.R
 import com.avenir.rangoapp.data.models.ProductModel
 import com.avenir.rangoapp.ui.theme.GrayColor
 
